d6e1c665e6719a79852ff31eb0abd00912bbf415
Rifqi2407 <rifqihida24@gmail.com>
2023-08-04 17:32:40 +0700

    first commit

---
+++ database/migrations/2023_08_02_001905_create_employees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->enum('jurusan', ['Teknik Elektro', 'Teknik Mesin', 'Teknik Sipil', 'Akuntansi', 'AB']);
+            $table->integer('nim');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
